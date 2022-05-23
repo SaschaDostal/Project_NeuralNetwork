f0ac87faf11109cc96c56a2c5d406f20d8451727
Sascha Dostal <91dosa1bif@hft-stuttgart.de>
2022-05-23 09:55:22 +0200

    add visualization of results

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -7,8 +7,9 @@ def main():
     input_layer_size = 2
     output_layer_size = 1
 
-    nn = create_nn(hidden_layer, hidden_layer_size, input_layer_size, output_layer_size)
+    #nn = create_nn(hidden_layer, hidden_layer_size, input_layer_size, output_layer_size)
     #nn = train_nn(nn, max_epochs, training_data)
+    plot()
 
 def create_nn(hidden_layer, hidden_layer_size, input_layer_size, output_layer_size):
     nn = []
@@ -41,8 +42,9 @@ def create_nn(hidden_layer, hidden_layer_size, input_layer_size, output_layer_si
 
     return nn
 
-def train_nn(nn, epochs, training_data):
-    # TODO: shuffle training_data
+def train_nn(nn, epochs, training_data, learning_rate):
+    
+    random.shuffle(training_data)
 
     for epoch in range(epochs):
         for sample in range(training_data):
@@ -56,10 +58,65 @@ def initialize_input_layer():
 
 def forward_pass(nn):
     for transition in nn:
+        pass
         
 
 def backward_pass():
     pass
+
+def plot():
+    import matplotlib.pyplot as plt
+    
+    # x-axis values
+    a = [1,2,3,4,5]
+    c = [6,7,8,9,10]
+    # y-axis values
+    b = [2,4,5,7,6]
+    d = [8,9,11,12,12]
+
+    precision = 200
+
+    max_x = 10
+    max_y = 12
+    
+    # color area for class 1
+    x_1 = []
+    y_1 = []
+    for x in range(precision):
+        for y in range(precision):
+            if test(x/float(precision) * max_x, y/float(precision) * max_y) == 0:
+                x_1.append(x/float(precision) * max_x)
+                y_1.append(y/float(precision) * max_y)
+    plt.scatter(x_1, y_1, color= "#630700", marker='s', s=50)
+
+    # color area for class 2
+    x_2 = []
+    y_2 = []
+    for x in range(precision):
+        for y in range(precision):
+            if test(x/float(precision) * max_x, y/float(precision) * max_y) == 1:
+                x_2.append(x/float(precision) * max_x)
+                y_2.append(y/float(precision) * max_y)
+    plt.scatter(x_2, y_2, color= "#000763", marker='s', s=50)
+
+    # draw points for test samples
+    plt.scatter(a, b, label= "Class 1", color= "red", s=10)
+    plt.scatter(c, d, label= "Class 2", color= "blue", s=10)
+    
+    # x-axis label
+    plt.xlabel('x - axis')
+    # y-axis label
+    plt.ylabel('y - axis')
+
+    plt.title('Visualization of test samples')
+    plt.legend()
+    plt.show()
+
+def test(x, y):
+    if x < 7 and y < 7:
+        return 0
+    else:
+        return 1
 
 if __name__ == "__main__":
     main()
