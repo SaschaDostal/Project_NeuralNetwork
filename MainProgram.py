ca897ed6413956e60693c586de617dd578dbdcfb
Sascha Dostal <91dosa1bif@hft-stuttgart.de>
2022-06-06 15:25:55 +0200

    add main program

---
+++ MainProgram.py
@@ -0,0 +1,28 @@
+import NeuralNetwork
+import Plotter
+
+import csv
+
+def main():
+    max_epochs = 100
+    learning_rate = 0.05
+    hidden_layers = 2
+    hidden_layer_size = 3
+    input_layer_size = 2
+    output_layer_size = 1
+
+    training_data = readCSV('data/testdata_train.csv')
+
+    nn = NeuralNetwork.NeuralNetwork(hidden_layers, hidden_layer_size, input_layer_size, output_layer_size)
+    nn.train(training_data, max_epochs, learning_rate)
+
+
+def readCSV(path):
+    data = []
+    with open(path, newline='') as f:
+        reader = csv.reader(f)
+        data.append(list(reader))
+    return data[0][1:]
+
+if __name__ == "__main__":
+    main()
