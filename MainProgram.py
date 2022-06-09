1ccb65569b3a9cdb5938225903bd6f8aa9d4ab30
Sascha Dostal <91dosa1bif@hft-stuttgart.de>
2022-06-09 23:06:24 +0200

    add options

--- MainProgram.py
+++ MainProgram.py
@@ -4,17 +4,42 @@ import Plotter
 import csv
 
 def main():
-    max_epochs = 100
-    learning_rate = 0.5
-    hidden_layers = 3
-    hidden_layer_size = 3   #includes bias
+    max_epochs = 1000
+    learning_rate = 0.6
+    hidden_layers = 4
+    hidden_layer_size = 4   #includes bias
     input_layer_size = 3    #includes bias
     output_layer_size = 1
 
-    training_data = readCSV('data/testdata_two.csv')
+    training_data = readCSV('data/testdata_train_2.csv')
 
-    nn = NeuralNetwork.NeuralNetwork(hidden_layers, hidden_layer_size, input_layer_size, output_layer_size)
-    nn.train(training_data, max_epochs, learning_rate)
+    answer = input("Load existing neural network? (y/n)\n")
+    if(answer == 'y'):
+        answer = input("Path/Filename: ")
+        nn = NeuralNetwork.NeuralNetwork(answer)
+    else:
+        nn = NeuralNetwork.NeuralNetwork(hidden_layers, hidden_layer_size, input_layer_size, output_layer_size)
+    
+    continue_training = True
+    while continue_training:
+        nn.train(training_data, max_epochs, learning_rate)
+        
+        Plotter.plot_learning_curve(nn.predicted_list, nn.epoch_list)
+        Plotter.plot_map(nn)
+        
+        answer = input("Continue training? (y/n)\n")
+        if(answer == 'y'):
+            answer = input("Choose new learning rate (currently: " + str(learning_rate) +"): ")
+            learning_rate = float(answer)
+        else:
+            answer = input("Save neural network? (y/n)\n")
+            if(answer == 'y'):
+                answer = input("Path/Filename: ")
+                nn.save_nn(answer)
+            continue_training = False
+
+    
+    
 
 
 def readCSV(path):
