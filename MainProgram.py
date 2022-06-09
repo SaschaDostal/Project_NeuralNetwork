a2d3fa96144aa98f3072b4a53cc9c6935350715a
Sascha Dostal <91dosa1bif@hft-stuttgart.de>
2022-06-09 23:25:34 +0200

    fix bug and format files

--- MainProgram.py
+++ MainProgram.py
@@ -3,12 +3,13 @@ import Plotter
 
 import csv
 
+
 def main():
     max_epochs = 1000
     learning_rate = 0.6
     hidden_layers = 4
-    hidden_layer_size = 4   #includes bias
-    input_layer_size = 3    #includes bias
+    hidden_layer_size = 4  # includes bias
+    input_layer_size = 3  # includes bias
     output_layer_size = 1
 
     training_data = readCSV('data/testdata_train_2.csv')
@@ -16,20 +17,22 @@ def main():
     answer = input("Load existing neural network? (y/n)\n")
     if(answer == 'y'):
         answer = input("Path/Filename: ")
-        nn = NeuralNetwork.NeuralNetwork(answer)
+        nn = NeuralNetwork.NeuralNetwork(path_to_nn=answer)
     else:
-        nn = NeuralNetwork.NeuralNetwork(hidden_layers, hidden_layer_size, input_layer_size, output_layer_size)
-    
+        nn = NeuralNetwork.NeuralNetwork(hidden_layers=hidden_layers, hidden_layer_size=hidden_layer_size,
+                                         input_layer_size=input_layer_size, output_layer_size=output_layer_size)
+
     continue_training = True
     while continue_training:
         nn.train(training_data, max_epochs, learning_rate)
-        
+
         Plotter.plot_learning_curve(nn.predicted_list, nn.epoch_list)
         Plotter.plot_map(nn)
-        
+
         answer = input("Continue training? (y/n)\n")
         if(answer == 'y'):
-            answer = input("Choose new learning rate (currently: " + str(learning_rate) +"): ")
+            answer = input(
+                "Choose new learning rate (currently: " + str(learning_rate) + "): ")
             learning_rate = float(answer)
         else:
             answer = input("Save neural network? (y/n)\n")
@@ -38,9 +41,6 @@ def main():
                 nn.save_nn(answer)
             continue_training = False
 
-    
-    
-
 
 def readCSV(path):
     data = []
@@ -48,6 +48,7 @@ def readCSV(path):
         reader = csv.reader(f)
         data.append(list(reader))
     return data[0][1:]
+
 
 if __name__ == "__main__":
     main()
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,31 +2,38 @@ import random
 import math
 import json
 
+
 class NeuralNetwork:
-    def __init__(self, hidden_layers, hidden_layer_size, input_layer_size, output_layer_size):
-        self.hidden_layers = hidden_layers
-        self.hidden_layer_size = hidden_layer_size
-        self.input_layer_size = input_layer_size
-        self.output_layer_size = output_layer_size
-        self.initialize_weights(self.hidden_layers, self.hidden_layer_size, self.input_layer_size, self.output_layer_size)
-    
-    def __init__(self, path_to_nn):
-        with open(path_to_nn) as data_file:
-            self.weights = json.load(data_file)
-        self.hidden_layers = len(self.weights) - 1
-        self.hidden_layer_size = len(self.weights[1])
-        self.input_layer_size = len(self.weights[0])
-        self.output_layer_size = len(self.weights[-1][0])
-        print(self.hidden_layers)
-        print(self.hidden_layer_size)
-        print(self.input_layer_size)
-        print(self.output_layer_size)
+    def __init__(self, path_to_nn=None, hidden_layers=2, hidden_layer_size=3, input_layer_size=3, output_layer_size=1):
+        print(path_to_nn)
+        if path_to_nn is None:
+            self.hidden_layers = hidden_layers
+            self.hidden_layer_size = hidden_layer_size
+            self.input_layer_size = input_layer_size
+            self.output_layer_size = output_layer_size
+            self.initialize_weights(
+                self.hidden_layers, self.hidden_layer_size, self.input_layer_size, self.output_layer_size)
+            print("Neural network initialized:")
+        else:
+            with open(path_to_nn) as data_file:
+                self.weights = json.load(data_file)
+            self.hidden_layers = len(self.weights) - 1
+            self.hidden_layer_size = len(self.weights[1])
+            self.input_layer_size = len(self.weights[0])
+            self.output_layer_size = len(self.weights[-1][0])
+            print("Neural network loaded:")
+
+        print("Hidden layers: " + str(self.hidden_layers))
+        print("Hidden layer size: " + str(self.hidden_layer_size))
+        print("Input layer size: " + str(self.input_layer_size))
+        print("Output layer size: " + str(self.output_layer_size))
 
     # creates a list with weights for every transition between two knots
     # Format: [number of transition][number of left knot][number of right knot]
+
     def initialize_weights(self, hidden_layer, hidden_layer_size, input_layer_size, output_layer_size):
         weights = []
-        for transitions in range (hidden_layer + 1):
+        for transitions in range(hidden_layer + 1):
             transition = []
             #print("TRANSITION - " + str(transitions))
 
@@ -42,7 +49,7 @@ class NeuralNetwork:
                 if transitions == hidden_layer:
                     num_right = output_layer_size
                 else:
-                    num_right = hidden_layer_size - 1 # "-1" wegen Bias-Knoten
+                    num_right = hidden_layer_size - 1  # "-1" wegen Bias-Knoten
 
                 for right_knot in range(num_right):
                     random_num = random.random()
@@ -51,8 +58,8 @@ class NeuralNetwork:
 
                 transition.append(left)
             weights.append(transition)
-        
-        self.weights=weights
+
+        self.weights = weights
 
     def train(self, training_data, epochs, learning_rate):
         self.predicted_list = []    # for graph
@@ -63,15 +70,15 @@ class NeuralNetwork:
             true = 0
             for sample in training_data:
                 output = self.forward_pass(sample)
-                total+=1
+                total += 1
                 if round(output[0]) == int(sample[-1]):
-                    true+=1
+                    true += 1
                 self.backward_pass(output, sample, learning_rate)
             self.predicted_list.append(float(true)/total)    # for graph
             self.epoch_list.append(epoch)        # for graph
             if epoch == 0 or epoch % 100 == 0:
-                print("Correct Predicted: {:7.4f}".format(float(true)/total) + "%")
-        
+                print("Correct Predicted: {:7.4f}".format(
+                    float(true)/total) + "%")
 
     def forward_pass(self, sample):
         output = sample[:-1]
@@ -80,26 +87,26 @@ class NeuralNetwork:
         #print("sample" + str(sample[:-1]))
         # for each transition
         for l in range(len(self.weights)):
-            output.append(1.0) # adding bias to output
+            output.append(1.0)  # adding bias to output
             self.currentOutput.append(output)
             out_ = []
             in_ = []
 
             # for each right knot in transition l
             for k in range(len(self.weights[l][0])):
-                input  = 0
+                input = 0
                 # calculate sum of outputs
                 for j in range(len(output)):
                     input += float(output[j]) * self.weights[l][j][k]
-                
+
                 in_.append(input)
                 out_.append(self.sig(input))
                 #print("input " + str(input) + " output " + str(self.sig(input)))
             #print("OUT" + str(out_))
-            
+
             self.currentInput.append(in_)
-            
-            output=out_
+
+            output = out_
         return output
 
     def backward_pass(self, output, sample, learning_rate):
@@ -109,7 +116,8 @@ class NeuralNetwork:
         # delta[k] = d-sig(in[k])*(y[k]-out[k])
         output_deltas = []
         for k in range(self.output_layer_size):
-            output_deltas.append((float(sample[k - self.output_layer_size]) - output[k]) * self.d_sig(self.currentInput[-1][k]))
+            output_deltas.append((float(
+                sample[k - self.output_layer_size]) - output[k]) * self.d_sig(self.currentInput[-1][k]))
             #print("Class:" + sample[k - self.output_layer_size] + ", Prediction: " + str(output[k]) + ", Diff: " + str(float(sample[k - self.output_layer_size])-output[k]))
         deltas.append(output_deltas)
 
@@ -118,19 +126,23 @@ class NeuralNetwork:
         #       delta[j] = g'(in[j]) * sum(w[j][k] * delta[k])
         for t in range(self.hidden_layers):
             hidden_deltas = []
-            for j in range(len(self.weights[-(1 + t)])-1):  # -1 because of bias knot
+            # -1 because of bias knot
+            for j in range(len(self.weights[-(1 + t)])-1):
                 sum = 0.0
                 for k in range(len(self.weights[-(1 + t)][j])):
                     sum += self.weights[-(1 + t)][j][k] * deltas[0][k]
-                hidden_deltas.append(sum * self.d_sig(self.currentInput[-(2 + t)][j]))
+                hidden_deltas.append(
+                    sum * self.d_sig(self.currentInput[-(2 + t)][j]))
             deltas.insert(0, hidden_deltas)
-        
+
         # for each w[j][k] do
         #   w[j][k] = w[j][k] + alpha * out[j] * delta[k]
         for t in range(len(self.weights)):
-            for j in range(len(self.weights[t])-1): # -1 because of bias knot
+            for j in range(len(self.weights[t])-1):  # -1 because of bias knot
                 for k in range(len(self.weights[t][j])):
-                    self.weights[t][j][k] = self.weights[t][j][k] + learning_rate * float(self.currentOutput[t][j]) * deltas[t][k]
+                    self.weights[t][j][k] = self.weights[t][j][k] + \
+                        learning_rate * \
+                        float(self.currentOutput[t][j]) * deltas[t][k]
 
     def sig(self, x):
         return 1 / (1 + math.exp(-x))
